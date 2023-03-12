2229c6096875876ac8f405e5870d45ba05fceb1f
Фиклистов Кирилл <mastervinaka@gmail.com>
2023-03-12 21:44:56 +0200

    commit

--- src/hooks/Hooks.ts
+++
@@ -1,6 +0,0 @@
-import { AppDispatch, RootState } from "@/redux/Store";
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-
-
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
